786cd1cc7a24bfcfc53864e93a18cb57eaedd8cd
root <971168718@qq.com>
2025-06-03 18:01:08 +0800

    update

--- HW_Lib/STM32H7/BSP/Bsp_GPIO.c
+++ HW_Lib/STM32H7/BSP/Bsp_GPIO.c
@@ -76,37 +76,27 @@ static IRQn_Type BspGPIO_GetExti_IRQnID(BspGPIO_Obj_TypeDef IO_Obj)
 {
     switch ((uint16_t)IO_Obj.pin)
     {
-    case GPIO_PIN_0:
-        return EXTI0_IRQn;
+        case GPIO_PIN_0: return EXTI0_IRQn;
+        case GPIO_PIN_1: return EXTI1_IRQn;
+        case GPIO_PIN_2: return EXTI2_IRQn;
+        case GPIO_PIN_3: return EXTI3_IRQn;
+        case GPIO_PIN_4: return EXTI4_IRQn;
+        case GPIO_PIN_5:
+        case GPIO_PIN_6:
+        case GPIO_PIN_7:
+        case GPIO_PIN_8:
+        case GPIO_PIN_9:
+            return EXTI9_5_IRQn;
 
-    case GPIO_PIN_1:
-        return EXTI1_IRQn;
+        case GPIO_PIN_10:
+        case GPIO_PIN_11:
+        case GPIO_PIN_12:
+        case GPIO_PIN_13:
+        case GPIO_PIN_14:
+        case GPIO_PIN_15:
+            return EXTI15_10_IRQn;
 
-    case GPIO_PIN_2:
-        return EXTI2_IRQn;
-
-    case GPIO_PIN_3:
-        return EXTI3_IRQn;
-
-    case GPIO_PIN_4:
-        return EXTI4_IRQn;
-
-    case GPIO_PIN_5:
-    case GPIO_PIN_6:
-    case GPIO_PIN_7:
-    case GPIO_PIN_8:
-    case GPIO_PIN_9:
-        return EXTI9_5_IRQn;
-
-    case GPIO_PIN_10:
-    case GPIO_PIN_11:
-    case GPIO_PIN_12:
-    case GPIO_PIN_13:
-    case GPIO_PIN_14:
-    case GPIO_PIN_15:
-        return EXTI15_10_IRQn;
-
-    default: debug_assert(true); break;
+        default: debug_assert(true); break;
     }
 
     return 0;
