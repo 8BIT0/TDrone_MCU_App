0806e4cd0fea97b419fbec862685a19960965c2f
root <971168718@qq.com>
2025-06-03 15:23:17 +0800

    PY_Tool update

--- PY_Tool/Mag_EllipsoidFitting.py
+++ PY_Tool/Mag_EllipsoidFitting.py
@@ -1,6 +1,7 @@
 import os
 import math
 import matplotlib.pyplot as plt
+from scipy.linalg import sqrtm
 import numpy as np
 
 def ThreeSigma_Check(data:list) -> list:
@@ -111,84 +112,107 @@ data_size = len(mag_x)
 
 # a1x^2 + a2y^2 + a3z^2 + a4xy + a5xz + a6yz + a7x + a8y + a9z = 1
 # x^2, y^2, z^2, xy, xz, yz, x, y, z
-D = np.array([(mag_x[i]**2, mag_y[i]**2, mag_z[i]**2, mag_x[i]*mag_y[i], mag_x[i]*mag_z[i], mag_y[i]*mag_z[i], mag_x[i], mag_y[i], mag_z[i]) for i in range(data_size)])
+D = np.array([(mag_x[i]**2, mag_y[i]**2, mag_z[i]**2, 2*mag_y[i]*mag_z[i], 2*mag_x[i]*mag_z[i], 2*mag_x[i]*mag_y[i], 2*mag_x[i], 2*mag_y[i], 2*mag_z[i], 1) for i in range(data_size)])
+
+S = D.T @ D
+
+# separate S
+S11 = S[0:6, 0:6]
+S12 = S[0:6, 6:10]
+S21 = S[6:10, 0:6]
+S22 = S[6:10, 6:10]
+
+print('[ S   matrix ]')
+print(S)
+print('[ S11 matrix ]')
+print(S11)
+print('[ S12 matrix ]')
+print(S12)
+print('[ S13 matrix ]')
+print(S21)
+print('[ S14 matrix ]')
+print(S22)
+
+C = [[-1, 1, 1, 0, 0, 0],
+     [1, -1, 1, 0, 0, 0],
+     [1, 1, -1, 0, 0, 0],
+     [0, 0, 0, -4, 0, 0],
+     [0, 0, 0, 0, -4, 0],
+     [0, 0, 0, 0, 0, -4]]
 
 # Least Squares
 # LS = (D.T * D)^-1 * D.T * I(unit matrix)
-a = np.linalg.pinv(D) @ np.ones(data_size)
-
-if len(a) != 9:
-    print(f'[ a Matrix size ERROR {len(a)} ]')
-    exit()
-
-print('[ A matrix ]')
-print(a)
-
-# create M(3 x 3) Positive Definite Matrix
-# M = [ a1,   a4/2, a5/2,
-#       a4/2, a2,   a6/2, 
-#       a5/2, a6/2, a3 ]
-M = np.array([[a[0], a[3]/2, a[4]/2],
-              [a[3]/2, a[1], a[5]/2],
-              [a[4]/2, a[5]/2, a[2]]])
-
-print('[ M matrix ]')
-print(M)
-
-try:
-    np.linalg.cholesky(M)
-except np.linalg.LinAlgError:
-    print('[ M matrix is not a Positive Definitr Matrix ]')
-    exit()
-
-# center = -(1/2)[a7, a8, a9]M^-1
-center = -0.5 * (np.array([a[6], a[7], a[8]]) @ np.linalg.inv(M))
-print('[ center ]')
-print(center)
-
-# scale factory
-# SS = C * M * C.T + 1
-Scale = (center @ M @ center.T + 1)/2500
-print('[ SS ]')
-print(Scale)
+M0 = np.linalg.inv(C) @ (S11 - S12 @ np.linalg.inv(S22) @ S12.T)
 
 #  separate M matrix to Value and Vector matrix
-values, vector = np.linalg.eig(M)
+values, vector = np.linalg.eig(M0)
 print('[ values ]')
 print(values)
 print('[ vector ]')
 print(vector)
 
-# get lambda
-Scale_Matrix = np.diag(np.sqrt(Scale / np.abs(values)))
+# find max value and index in diagonal matrix
+max_val = np.max(np.diag(values))
+max_index = np.argmax(np.diag(values))
+print('[ max value ]')
+print(max_val)
+print('[ max index ]')
+print(max_index)
 
-print('[ Scale Matrix ]')
-print(Scale_Matrix)
+v1 = vector[:,max_index]
+print('[ v1 ]')
+print(v1)
+if v1[0] < 0:
+    v1 = -v1
 
-# soft iron matrix
-soft_iron_matrix = Scale_Matrix @ vector.T
+v2 = -np.linalg.pinv(S22) @ S12.T @ v1
+
+# create M(3 x 3) Positive Definite Matrix
+# M = [ a1,   a4/2, a5/2,
+#       a4/2, a2,   a6/2, 
+#       a5/2, a6/2, a3 ]
+M = np.array([[v1[0],   v1[3]/2, v1[4]/2],
+              [v1[3]/2, v1[1],   v1[5]/2],
+              [v1[4]/2, v1[5]/2, v1[2]]])
+n = v2[0:3]
+d = v2[3]
+
+print('[ M matrix ]')
+print(M)
+
+# get center
+center = -np.linalg.inv(M) @ n
+
+print('[ center ]')
+print(center)
+
+# get soft iron matrix
+soft_iron_matrix = sqrtm(M)
+
 print('[ soft iron matrix ]')
 print(soft_iron_matrix)
 
 # fix mag data
-# mag_fix = soft_iron_matrix * (mag - center)
 mag_org = np.array([(mag_x_org[i], mag_y_org[i], mag_z_org[i]) for i in range(data_size)])
+mag_shift = mag_org - center
+mag_fix = np.array([(soft_iron_matrix @ mag_shift[i]) for i in range(data_size)])
 
-# shift center
-mag_shift = []
-for i in range(data_size):
-    # shift = soft_iron_matrix @ (mag_org[i] - center)
-    shift = soft_iron_matrix @ mag_org[i]
-    mag_shift.append(shift)
+mag_shift_x = [m_d[0] for m_d in mag_shift]
+mag_shift_y = [m_d[1] for m_d in mag_shift]
+mag_shift_z = [m_d[2] for m_d in mag_shift]
 
-mag_shift_x = [d[0] for d in mag_shift]
-mag_shift_y = [d[1] for d in mag_shift]
-mag_shift_z = [d[2] for d in mag_shift]
+mag_fix_x = [m_d[0] for m_d in mag_fix]
+mag_fix_y = [m_d[1] for m_d in mag_fix]
+mag_fix_z = [m_d[2] for m_d in mag_fix]
 
 # plot raw and fix data in same figure
 create_plot(mag_x_org, mag_shift_x, 'Mag X Shift', 'Sample point', 'X', 'red')
 create_plot(mag_y_org, mag_shift_y, 'Mag Y Shift', 'Sample point', 'Y', 'red')
 create_plot(mag_z_org, mag_shift_z, 'Mag Z Shift', 'Sample point', 'Z', 'red')
+
+create_plot(mag_x_org, mag_fix_x, 'Mag X fix', 'Sample point', 'X', 'red')
+create_plot(mag_y_org, mag_fix_y, 'Mag Y fix', 'Sample point', 'Y', 'red')
+create_plot(mag_z_org, mag_fix_z, 'Mag Z fix', 'Sample point', 'Z', 'red')
 
 fig = plt.figure(figsize=(6, 4))
 ax = fig.add_subplot(111, projection='3d')
@@ -200,7 +224,7 @@ ax.set_title('mag figure', fontsize=15)
 ax.set_xlabel('X', fontsize=12)
 ax.set_ylabel('Y', fontsize=12)
 ax.set_zlabel('Z', fontsize=12)
-scatter = ax.scatter(mag_shift_x, mag_shift_y, mag_shift_z, c='blue')
+scatter = ax.scatter(mag_fix_x, mag_fix_y, mag_fix_z, c='blue')
 scatter = ax.scatter(mag_x_org, mag_y_org, mag_z_org, c='red')
 plt.tight_layout()
 
